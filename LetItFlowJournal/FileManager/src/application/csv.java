5041fc850a8d7cf299c3eef4b67ff1465913c2cf
Julianus Aurelius <61988360+JulianusAurelius@users.noreply.github.com>
2022-07-25 22:10:29 -0500

    Break up FM into smaller classes
    Fix multiple bugs and incorrect implementations

---
+++ LetItFlowJournal/FileManager/src/application/csv.java
@@ -0,0 +1,227 @@
+import java.io.*;
+import java.time.LocalDate;
+import java.util.Random;
+import java.util.ArrayList;
+
+public class csv {
+    // csv file format: filename,date created,date modified
+    
+    public static void NewNote(String filename, String user, String password) {
+        LocalDate date = LocalDate.now();
+        String dateString = date.toString();
+        String entry = filename + "," + dateString + "," + dateString + "\n"; // Can change to storage directory
+        // String csvpath = user + ".csv";
+        try {
+            String csvFile = file.Read(user, password, "csv");
+            csvFile = csvFile.strip() + "\n";
+            csvFile += entry;
+            // System.out.println(csvFile);
+            file.Save(user, csvFile, password, user, "csv");
+        } catch (Exception e) {
+            System.out.println("Error adding file to csv"); // FIX
+        }
+    }
+
+
+
+    public static void DeleteNote(String filename, String user, String password) {
+        String csvpath = user + ".csv";
+        // loop through csv lines and look for filename in first column
+        // if found, delete that line
+        try {
+            // make temp file
+            String temp_string = "temp";
+            java.util.Random rand = new Random();
+            while (file.detectCollision(temp_string, "csv")) { // in case a user names themselves temp or something similar. This tries to avoid collision and be dynamic
+                temp_string += Integer.toString(rand.nextInt(10));
+                if (temp_string.length() > 20) {
+                    temp_string = "temp";
+                }
+            }
+            java.io.File temp = new java.io.File(temp_string + ".csv");
+            java.io.FileWriter tempWriter = new java.io.FileWriter(temp);
+            // java.io.FileReader csv = new java.io.FileReader(csvpath);
+            // java.io.BufferedReader reader = new java.io.BufferedReader(csv);
+            String file_text = file.Read(user, password, "csv");
+            // read csv file line by line, then split the string on ','
+            // check if first token is filename, if so, delete that line
+            String[] lines = file_text.split("\n");
+            for (int i = 0; i < lines.length; i++) {
+                String[] tokens = lines[i].split(",");
+                if (tokens[0].equals(filename)) {
+                    continue;
+                } else {
+                    tempWriter.write(lines[i] + "\n");
+                }
+            }
+            // replace csv file with temp file
+            temp.renameTo(new java.io.File(csvpath));
+            tempWriter.close();
+            // csv.close();
+            // reader.close();
+
+        } catch (Exception e) {
+            System.out.println("Error deleting file"); // FIX
+        }
+    }
+
+
+
+    public static void changeDate(String filename, String username, String password) {
+        // String csvpath = username + ".csv";
+        try {
+            String[] csvText = file.Read(username, password, "csv").split("\n");
+            String temptxt = username + "\n";
+            for (int i = 1; i < csvText.length; i++) {
+                if (csvText[i].split(",")[0].equals(filename)) {
+                    temptxt += filename + "," + csvText[i].split(",")[1] + "," + LocalDate.now().toString() + "\n";
+                } else {
+                    temptxt += csvText[i] + "\n";
+                }
+            }
+            // // make temp csv file
+            String temp_string = "temp";
+            java.util.Random rand = new Random();
+            while (file.detectCollision(temp_string, "csv")) { // in case a user names themselves temp or something similar. This tries to avoid collision and be dynamic
+                temp_string += Integer.toString(rand.nextInt(10));
+                if (temp_string.length() > 20) {
+                    temp_string = "temp";
+                }
+            }
+            java.io.File temp = new java.io.File(temp_string);
+            java.io.FileWriter tempWriter = new java.io.FileWriter(temp);
+
+            temptxt = file.encrypt(temptxt, password);
+
+            // replace csv file with temp file
+            tempWriter.write(temptxt);
+            temp.renameTo(new java.io.File(username + ".csv"));
+            tempWriter.close();
+            // csv.close();
+            // reader.close();
+        } catch (Exception e) {
+            System.out.println("Error changing file"); // FIX
+        }
+    }
+
+
+
+
+    public static void ChangeFile(String oldFilename, String newFilename, String username, String password) {
+        // String csvpath = username + ".csv";
+        try {
+            String[] csvText = file.Read(username, password, "csv").strip().split("\n", -1);
+            // System.out.println(csvText.split("\n")[1]);
+            String temptxt = username + "\n";
+            for (int i = 1; i < csvText.length; i++) {
+                if (csvText[i].split(",")[0].equals(oldFilename)) {
+                    temptxt += newFilename + "," + csvText[i].split(",")[1] + "," + LocalDate.now().toString() + "\n";
+                } else {
+                    temptxt += csvText[i] + "\n";
+                }
+            }
+            // java.io.FileReader csv = new java.io.FileReader(csvpath);
+            // java.io.BufferedReader reader = new java.io.BufferedReader(csv);
+            // // make temp csv file
+            String temp_string = "temp";
+            java.util.Random rand = new Random();
+            while (file.detectCollision(temp_string, "csv")) { // in case a user names themselves temp or something similar. This tries to avoid collision and be dynamic
+                temp_string += Integer.toString(rand.nextInt(10));
+                if (temp_string.length() > 20) {
+                    temp_string = "temp";
+                }
+            }
+            java.io.File temp = new java.io.File(temp_string);
+            java.io.FileWriter tempWriter = new java.io.FileWriter(temp);
+
+            temptxt = file.encrypt(temptxt, password);
+
+            // replace csv file with temp file
+            tempWriter.write(temptxt);
+            temp.renameTo(new java.io.File(username + ".csv"));
+            tempWriter.close();
+
+            File file = new File(username + "_" + oldFilename + ".txt");
+            file.renameTo(new File(username + "_" + newFilename + ".txt"));
+        } catch (Exception e) {
+            System.out.println("Error changing file"); // FIX
+        }
+    }
+
+
+
+
+    public static String NewUser(String username, String password) { // NEEDS TESTING
+        // create new csv file with username and encrypted with password
+        // return either "success" or "error"
+        if (file.detectCollision(username, "csv")) {
+            return "Error: User already exists";
+        }
+        try {
+            File temp = new File(username + ".csv");
+            temp.createNewFile();
+            FileWriter tempWriter = new FileWriter(username + ".csv");
+            tempWriter.write(file.encrypt(username + "\n", password));
+            tempWriter.close();
+            return "Success";
+        } catch (Exception e) {
+            return "Error creating user";
+        }
+    }
+
+
+
+
+    public static Boolean verifyUser(String username, String password) { // Need to work on errors to make them more security aligned (not as informative to error)
+        // NEEDS TESTING
+        if (file.detectCollision(username, "csv")) {
+            String csvFile = file.Read(username, password, "csv");
+            // compare first line to username
+            if (csvFile.split("\n")[0].equals(username)) {
+                return true; // "LOGIN";
+            } else {
+                return false; // "Error: Incorrect username or password";
+            }
+        }
+        else {
+            return false; // "Error: User does not exist";
+        }
+    }
+
+
+
+    public static ArrayList<String> Load(String username, String password) {
+        if (file.detectCollision(username, "csv")) {
+            ArrayList<String> notes = new ArrayList<String>();
+            String csvFile = file.Read(username, password, "csv");
+            String[] csvText = csvFile.split("\n");
+            for (int i = 1; i < csvText.length; i++) {
+                notes.add(csvText[i].split(",")[0]);
+            }
+            return notes;
+        }
+        else {
+            ArrayList<String> error = new ArrayList<String>();
+            return error;
+        }
+    }
+
+
+
+    public static void DeleteUser(String username, String password) { // NEEDS TESTING
+        // loop through csv file and delete all files with username_name.txt
+        // then delete csv file // NEVERMIND, the DeleteNote() does that automatically
+        String csvpath = username + ".csv";
+        try {
+            String[] csvText = file.Read(username, password, "csv").split("\n");
+            for (int i = 1; i < csvText.length; i++) {
+                note.DeleteNoteFromCSV(csvText[i].split(",")[0], username);
+            }
+            java.io.File csv = new java.io.File(csvpath);
+            csv.delete();
+        } catch (Exception e) {
+            System.out.println("Error deleting user"); // FIX
+        }
+    }
+
+}
---
+++ LetItFlowJournal/FileManager/src/application/file.java
@@ -0,0 +1,110 @@
+import java.io.File;
+import java.nio.file.Files;
+
+
+public class file {
+
+
+    public static Boolean detectCollision(String filename, String ext) {
+        return new java.io.File(filename + "." + ext).exists();
+    }
+
+
+
+    public static String encrypt(String text, String key) {
+        // custom encrypt function so we don't need to worry about imports and dependencies
+        // Plus we aren't worrying about serious cybersecurity, but rather privacy from most people
+
+        // xor each character in the file with the key
+        // then return the encrypted string
+        String encrypted = "";
+        for (int i = 0; i < text.length(); i++) {
+            encrypted += (char) (text.charAt(i) ^ key.charAt(i % key.length()));
+        }
+        return encrypted;
+    }
+
+
+
+    public static String decrypt(String text, String key) {
+        // custom decrypt function so we don't need to worry about imports and dependencies
+        // Plus we aren't worrying about serious cybersecurity, but rather privacy from most people
+
+        // xor each character in the file with the key
+        // then return the decrypted string
+        String decrypted = "";
+        for (int i = 0; i < text.length(); i++) {
+            decrypted += (char) (text.charAt(i) ^ key.charAt(i % key.length()));
+        }
+        return decrypted;
+    }
+
+
+    public static String Read(String filename, String password, String ext) {
+        if (file.detectCollision(filename, ext)) { // FIX
+            String txtpath = filename + "." + ext;
+            String text = "";
+            String fileContent = "";
+            try {
+                // java.io.FileReader txt = new java.io.FileReader(txtpath);
+                // java.io.BufferedReader reader = new java.io.BufferedReader(txt);
+                // String line;
+                // while ((line = reader.readLine()) != null) {
+                //     text += line + "\n"; // will add a newline at the end of the file
+                // }
+                // reader.close();
+                // txt.close();
+                fileContent = Files.readString(new File(txtpath).toPath());
+            } catch (Exception e) {
+                System.out.println("Error loading file"); // FIX
+            }
+            String decryptedText = decrypt(fileContent, password);
+            return decryptedText;
+        } else {
+            System.out.println("File does not exist"); // FIX
+            return "";
+        }
+    }
+
+
+
+    public static void ChangeFilename(String oldFilename, String newFilename, String username, String password) {
+        if (detectCollision(username + "_" + oldFilename, "txt")) {
+            if (detectCollision(username + "_" + newFilename, "txt")) {
+                System.out.println("File already exists with that name"); // FIX
+            } else {
+                File oldFile = new File(username + "_" + oldFilename + ".txt");
+                File newFile = new File(username + "_" + newFilename + ".txt");
+                if (oldFile.renameTo(newFile)) {
+                    csv.ChangeFile(oldFilename, newFilename, username, password);
+                } else {
+                    System.out.println("Error renaming file"); // FIX
+                }
+            }
+        } else {
+            System.out.println("File does not exist"); // FIX
+        }
+    }
+
+
+
+    public static void Save(String filename, String text, String password, String username, String ext) {
+        if (detectCollision(filename, ext)) {
+            String txtpath = filename + "." + ext;
+            String encrypted = encrypt(text.strip(), password);
+            try {
+                java.io.FileWriter txtFile = new java.io.FileWriter(txtpath);
+                txtFile.write(encrypted);
+                txtFile.close();
+                if (!ext.equals("csv")) {
+                    csv.changeDate(filename, username, password);
+                }
+            } catch (Exception e) {
+                System.out.println("Error saving file"); // FIX
+            }
+        } else {
+            System.out.println("File does not exist"); // FIX
+        }
+    }
+
+}
